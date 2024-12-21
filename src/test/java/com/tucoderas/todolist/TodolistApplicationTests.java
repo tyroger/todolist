9710d2bbcfd9c12ac9eed843a9d4f79d248baefe
Roger Tuyishime <tyroger@pc-113.home>
2024-12-21 10:19:11 +0100

    init a new java project

---
+++ src/test/java/com/tucoderas/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tucoderas.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
